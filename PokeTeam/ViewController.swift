aab69cebac8e6b4187fa1c123af8dc1c70e61127
Uto <daffamadeo@gmail.com>
2022-04-27 17:18:27 +0700

    Initial Commit

---
+++ PokeTeam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeTeam
+//
+//  Created by Tb. Daffa Amadeo Zhafrana on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
